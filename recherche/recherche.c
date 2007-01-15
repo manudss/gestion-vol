206538bc89892f4d46361d2c7b4ead12d9623f3c
manu.dss <manu.dss@76d09d79-1b25-0410-b0ee-4f19602f6bda>
2007-01-15 00:14:05 +0000

    correction d'ereur dans liste.c fontion de recherche avec fonction global pour chercher un client et qui marche.

--- recherche/recherche.c
+++ recherche/recherche.c
@@ -6,22 +6,67 @@
 
 
 
+//--------------------------- Fonction Recherche --------------------------------------------------------------//
+
 void * recherche(void *  achercher,llist liste, void * (fct_rech) (void* achercher,void * chercherdans))
 {
 
 
     element* tmp = liste;
+    char * val;
 
     int ok =0;
 	void * cherche = NULL;
+    DBG
+    val = (char *) achercher;
+    printf("recherche ... ");
+    puts(val);
 
-    while(tmp != NULL && ok != 1 )
+    while(liste != NULL && ok != 1 )
     {
-		cherche = fct_rech(achercher, liste.data);
+        DBG
+		cherche = fct_rech(achercher, liste->data);
 
-    	if(cherche != NULL) ok=1;
-		else liste = liste.suiv;
+    	if(cherche != NULL)
+    	{
+    	    DBG
+    	    tmp = (llist) cherche;
+    	    //puts(tmp->cle);
+    	    ok=1;
+    	    return liste->data;
+    	}
+		else
+		{
+		    DBG
+		    liste = liste->suiv;
+		    DBG
+		}
 
     }
-    return cherche;
+    DBG
+    return liste;
+}
+
+void * recherch_client_par_cle(void* achercher,void * chercherdans)
+{
+    char * cle;
+    ptr_t_client client;
+
+    cle = (char*) achercher;
+    puts(cle);
+    client = (ptr_t_client) chercherdans;
+    DBG
+    puts(client->cle);
+    DBG
+    if (strcmp(cle , client->cle) == 0 )
+    {
+        DBG
+        return chercherdans;
+    }
+    else
+    {
+        DBG
+        return NULL;
+    }
+
 }
